bb5096a8fcc38ede5d2e453833c35a228d05aa09
Gogi4aaa <georgi-07@abv.bg>
2025-02-23 22:21:49 +0200

    changedLoginDesign

--- MyFriend/src/Interfaces/Login/Login.ts
+++ MyFriend/src/Interfaces/Login/Login.ts
@@ -1,4 +1,4 @@
 export interface LoginCredentials {
-    email: string;
+    name: string;
     password: string;
 }
--- MyFriend/src/components/Login/Login.tsx
+++ MyFriend/src/components/Login/Login.tsx
@@ -2,13 +2,20 @@ import { useState } from "react";
 import "../Login/Login.css"
 import { LoginCredentials } from "../../Interfaces/Login/Login";
 import axios from "axios";
+import SoundSelector from "../SoundSelector/SoundSelector";
+let selectedSound: any;
 export const Login = () => {
-    const [email, setEmail] = useState("");
+  
+    const [name, setName] = useState("");
     const [password, setPassword] = useState("");
-
+    const soundFiles = [
+      "/sounds/sound1.mp3",
+      "/sounds/sound2.mp3",
+      "/sounds/sound3.mp3",
+    ];
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        await login({ email, password });
+        await login({ name, password });
       };
     const login = async (credentials: LoginCredentials): Promise<void> => {
     try {
@@ -23,23 +30,74 @@ export const Login = () => {
         console.error("Login failed:", error);
     }
     };
+    const showProfileInfo = () => {
+        
+    };
     return(
-        <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <button type="submit">Login</button>
-    </form>
+    //     <form onSubmit={handleSubmit}>
+    //   <input
+    //     type="email"
+    //     placeholder="Email"
+    //     value={email}
+    //     onChange={(e) => setEmail(e.target.value)}
+    //     required
+    //   />
+    //   <input
+    //     type="password"
+    //     placeholder="Password"
+    //     value={password}
+    //     onChange={(e) => setPassword(e.target.value)}
+    //     required
+    //   />
+    //   <button type="submit">Login</button>
+    // </form>
+    
+    <>
+      <header>
+        <nav>
+            <div className="logo-container">
+                <img src="logo.png" alt="My friend" width="85" height="80" />
+            </div>
+            <div className="nav-buttons">
+                <button >Начало</button>
+                <button id="profile-button">Вход / Регистрация</button>
+                <button id="games-button" disabled>Игри</button>
+                <button id="profile-info-button" style={{display: "none"}} onClick={showProfileInfo}>Профил</button>
+                <button id="logout-button" style={{display: "none"}} >Изход</button>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div className="clouds">
+            <div className="cloud cloud1"></div>
+            <div className="cloud cloud2"></div>
+            <div className="cloud cloud3"></div>
+        </div>
+        <div className="canvas">
+            <div className="logo-container">
+                <img src="logo.png" alt="My friend" width="85" height="80" />
+            </div>
+            <h1>Регистрация/ Вход</h1>
+            <input
+              type="name"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <input type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required 
+            />
+            <div className="sound-selection">
+                <SoundSelector soundFiles={soundFiles}/>
+            </div>
+            <button id="register-button" onClick={handleSubmit}>Регистрация</button>
+        </div>
+    </main>
+    </>
     )
 }
