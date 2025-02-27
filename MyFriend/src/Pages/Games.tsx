2bf8699f50765599cda9a4e7223415b031c1a774
Gogi4aaa <georgi-07@abv.bg>
2025-02-27 19:56:39 +0200

    added 2 games to the project

--- MyFriend/src/Pages/Games.tsx
+++ MyFriend/src/Pages/Games.tsx
@@ -1,9 +1,12 @@
-import AnimalSoundGame from "../components/games/AnimalGame"
-
+import Game from "../components/games/Game";
+import img2 from "../assets/images/2.png";
+import img3 from "../assets/images/3.png";
+import img4 from "../assets/images/4.png";
 export const Games = () => {
     return (
         <>
-            <AnimalSoundGame />      
+            <Game img={img2} link="https://jenijivkova.itch.io/animalsoundgame"/>
+            <Game img={img3} link="https://jenijivkova.itch.io/storygame"/>
         </>
     )
 }
